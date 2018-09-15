35ec82a63ca4718f2b4d858610ce8f66954bd32f
JRihn32 <justin.rihn32@gmail.com>
2018-09-15 10:26:16 -0500

    set up project with most dependencies

---
+++ src/main/java/com/progression/progressioncapstone/ProgressionCapstoneApplication.java
@@ -0,0 +1,12 @@
+package com.progression.progressioncapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressionCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgressionCapstoneApplication.class, args);
+    }
+}
