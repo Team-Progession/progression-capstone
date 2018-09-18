156c04d67fe071b6551935e47f65913f48707105
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-09-18 13:42:31 -0500

    added authentication

--- src/main/java/com/progression/progressioncapstone/Models/User.java
+++ src/main/java/com/progression/progressioncapstone/Models/User.java
@@ -12,6 +12,14 @@ import javax.persistence.Table;
 @Table(name = "users")
 public class User {
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
+
     @Id
     @GeneratedValue
     private long id;
