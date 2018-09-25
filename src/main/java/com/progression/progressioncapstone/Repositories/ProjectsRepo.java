9bd539ecf63dd018aee682bbed0b56af4f6b6fac
JRihn32 <justin.rihn32@gmail.com>
2018-09-25 10:10:50 -0500

    linked projects to users

--- src/main/java/com/progression/progressioncapstone/Repositories/ProjectsRepo.java
+++ src/main/java/com/progression/progressioncapstone/Repositories/ProjectsRepo.java
@@ -1,12 +1,17 @@
 package com.progression.progressioncapstone.Repositories;
 
 import com.progression.progressioncapstone.Models.Project;
- import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectsRepo extends CrudRepository<Project, Long> {
     Project findAllByOwner(String title);
 
+    @Query(nativeQuery= true, value="SELECT * FROM projects where owner like ?1")
+    List<Project> findAllByOwner(long id);
 }
