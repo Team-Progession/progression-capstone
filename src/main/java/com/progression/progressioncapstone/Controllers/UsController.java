2c5954badc00c0b6a3c53e432277195934a49d0c
LindaHerrera <lindaherrera.marie@gmail.com>
2018-09-28 16:43:04 -0500

    about us page added

---
+++ src/main/java/com/progression/progressioncapstone/Controllers/UsController.java
@@ -0,0 +1,18 @@
+package com.progression.progressioncapstone.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsController {
+
+    @GetMapping("/us")
+    public String returnAboutUsPage() {
+        return "/fragments/about-us";
+    }
+
+}
+
+
+
